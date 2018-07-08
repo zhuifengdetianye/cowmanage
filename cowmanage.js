38fb2243d99aa515f8349e7b8d75c9020120a087
unknown <nianhuawuxiaoxin@yeah.net>
2018-07-09 01:22:11 +0800

    20180709

---
+++ cowmanage.js
@@ -0,0 +1,38 @@
+var express = require('express')
+var app = express()
+
+//设置handlebars的视图引擎
+var handlebars = require('express3-handlebars')
+        .create({defaultLayout:'main'})
+app.engine('handlebars', handlebars.engine)
+app.set('view engine', 'handlebars')
+
+app.set('port', process.env.PORT || 3000)
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function (req, res) {
+    res.render('home')
+})
+
+app.get('/about', function (req, res) {
+    res.render('about')
+})
+//定制404页面
+app.use(function (req, res) {
+    res.status(404)
+    res.render('404')
+})
+
+//定制500页面
+app.use(function(err, req, res, next){
+    console.error(err.stack)
+    res.type('text/plain')
+    res.status(500)
+    res.render('500')
+})
+
+app.listen(app.get('port'), function () {
+    console.log('Express started on http://localhost:'+
+        app.get('port') + '; press Ctrl-C to terminate')
+})
